2ee34304eb2bae33b50859e72590ee5aea76f4c3
yoavip <yoavip@gmail.com>
2026-08-02 00:04:44 +0300

    Refactor Index component and update text

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const App = () => {
   return (
-    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "50px" }}>
+    <div>
       <h1>Hello from Sauna Kapara Booking!</h1>
-      <p>If you can see this text, the deployment works 🎉</p>
+      <p>If you can see this, deployment works 🎉</p>
     </div>
   );
 };
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+export default App;
